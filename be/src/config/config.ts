7055168ba23c401d0c087776adee2ff954ecfef8
saifaldin3388 <saifaldin3388@gmail.com>
2023-05-24 23:25:00 +0300

    finalize dockerfiles

--- be/src/config/config.ts
+++ be/src/config/config.ts
@@ -1,12 +1,12 @@
 export const config = () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
   database: {
-    host: process.env.MONGODB_HOST,
+    host: process.env.MONGODB_HOST || 'mongodb://localhost/emailer',
   },
   aws: {
-    endpoint: process.env.AWS_ENDPOINT,
-    id: process.env.AWS_ID,
-    secret: process.env.AWS_SECRET,
-    region: process.env.AWS_REGION,
+    endpoint: process.env.AWS_ENDPOINT || 'http://localhost:4566',
+    region: process.env.AWS_REGION || 'us-east-1',
+    id: process.env.AWS_ID || 'id',
+    secret: process.env.AWS_SECRET || 'secret',
   },
 });
