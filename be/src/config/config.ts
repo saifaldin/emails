c719ce89e2b13e438d9f2ea5306c77f9c40dbc5b
saifaldin3388 <saifaldin3388@gmail.com>
2023-05-24 23:58:04 +0300

    finalize docker compose

--- be/src/config/config.ts
+++ be/src/config/config.ts
@@ -1,7 +1,7 @@
 export const config = () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
   database: {
-    host: process.env.MONGODB_HOST || 'mongodb://localhost/emailer',
+    host: process.env.MONGODB_HOST || 'mongodb://mongo:27017/emailer',
   },
   aws: {
     endpoint: process.env.AWS_ENDPOINT || 'http://localhost:4566',
